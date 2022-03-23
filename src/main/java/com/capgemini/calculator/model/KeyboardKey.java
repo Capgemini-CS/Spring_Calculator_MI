454dc89d15ba1290760824a75305b0d92b9ddac8
IMARGHIO <ioan.marghioala@capgemini.com>
2022-03-23 15:16:22 +0200

    Added model and exception for input numbers

--- src/main/java/com/capgemini/calculator/model/KeyboardKey.java
+++ src/main/java/com/capgemini/calculator/model/KeyboardKey.java
@@ -1,11 +1,22 @@
 package com.capgemini.calculator.model;
 
+import com.capgemini.calculator.exception.ExceptionWhenKeyIsInvalid;
+
+import java.util.Scanner;
+
 public class KeyboardKey {
+
     public KeyboardKey() {
     }
 
-    public int getKeyNumber() {
-        int key = 0;
+    public int getKeyNumber() throws ExceptionWhenKeyIsInvalid {
+        int key;
+        Scanner scanner = new Scanner(System.in);
+        try{
+            key = scanner.nextInt();
+        } catch (NumberFormatException number) {
+            throw new ExceptionWhenKeyIsInvalid("Your parameter is not a valid key.");
+        }
         return key;
     }
 }
